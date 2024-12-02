cfa5bd0bbe45ff002879fe56da1ba5f20611faa6
Zvezdomir Dalev <zvezdomir.dalev@gmail.com>
2024-12-02 18:47:42 +0200

    style: make format codebase

--- src/advent_of_code_2024/day1.py
+++ src/advent_of_code_2024/day1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-def read_input(file_path: Path)->list:
+def read_input(file_path: Path) -> list:
     read_data = file_path.read_text()
     left_side = []
     right_side = []
@@ -9,18 +9,20 @@ def read_input(file_path: Path)->list:
         _line = line.split()
         left_side.append(_line[0].strip())
         right_side.append(_line[1].strip())
-    result = [left_side,right_side]
+    result = [left_side, right_side]
     return result
 
 
-
-def get_distance_between_numbers(left_side:list,right_side:list)->list:
+def get_distance_between_numbers(left_side: list, right_side: list) -> list:
     left_side.sort()
     right_side.sort()
 
-    result = list(map(lambda x: abs(int(x[0])-int(x[1])), zip(left_side,right_side)))
+    result = list(
+        map(lambda x: abs(int(x[0]) - int(x[1])), zip(left_side, right_side)),
+    )
 
     return result
+
 
 if __name__ == '__main__':
     data = read_input(Path('inputs/day1_inputs'))
