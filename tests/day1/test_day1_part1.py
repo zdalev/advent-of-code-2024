f8e08a1ff15a58fd03e5237b9a7c6f7959eb58a5
Zvezdomir Dalev <zvezdomir.dalev@gmail.com>
2024-12-02 18:48:00 +0200

    reformatiing

--- tests/day1/test_day1_part1.py
+++ tests/day1/test_day1_part1.py
@@ -5,8 +5,7 @@ from advent_of_code_2024.day1 import get_distance_between_numbers
 
 def test_how_far_apart_are_2_numbers():
     actual = get_distance_between_numbers(
-        ['3', '4', '2', '1', '3', '3'], [
-        '4', '3', '5', '3', '9', '3',
-        ],
+        ['3', '4', '2', '1', '3', '3'],
+        ['4', '3', '5', '3', '9', '3'],
     )
     assert sum(actual) == 11
